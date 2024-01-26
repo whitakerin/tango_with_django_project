0e3ce27d9b0276412a0c87e06fb63c4c511a260f
Erin Whitaker <2736368w@student.gla.ac.uk>
2024-01-26 17:44:09 +0000

    chapter 4 done

--- rango/urls.py
+++ rango/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from django.urls import include
 from rango import views
 
+app_name = 'rango'
+
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('about/', views.about, name ='about'),
